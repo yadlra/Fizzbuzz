5eb1e01562283bc3890eeb5df3274e836a81df58
Hannah Frost <h.7frost@gmail.com>
2019-03-26 12:52:58 +0000

    Fizzbuzz completed

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,3 +1,9 @@
 def fizzbuzz(number)
-  'fizz'
+  if number % 5 == 0 && number % 3 == 0
+    return "fizzbuzz"
+  elsif number % 5 == 0
+    return "buzz"
+  elsif number % 3 == 0
+    return "fizz"
+  end
 end
