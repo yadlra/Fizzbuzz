14cdd77c75e710da39b76d4ed9854257051b37e9
yadlra <yadi.sb1@gmail.com>
2019-03-26 13:13:09 +0000

    More refactoring

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,5 +1,5 @@
 def fizzbuzz(number)
-  return "fizzbuzz" if number % 5 == 0 && number % 3 == 0
-  return "buzz" if number % 5 == 0
-  return "fizz" if number % 3 == 0
+  return "FIZZBUZZ" if number % 5 == 0 && number % 3 == 0
+  return "BUZZ" if number % 5 == 0
+  return "FIZZ" if number % 3 == 0
 end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,12 +1,12 @@
 require 'fizzbuzz'
 describe 'fizzbuzz' do
-  it 'returns "fizz" when passed 3' do
+  it 'returns "FIZZ" when passed 3' do
     expect(fizzbuzz(3)).to eq 'fizz'
   end
-  it 'returns "buzz" when passed 5' do
+  it 'returns "BUZZ" when passed 5' do
     expect(fizzbuzz(5)).to eq 'buzz'
   end
-  it 'returns "fizzbuzz" when passed 15' do
+  it 'returns "FIZZBUZZ" when passed 15' do
     expect(fizzbuzz(15)).to eq 'fizzbuzz'
   end
 end
